a13bae088138ec783fe43055f4a392447ff37738
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-03-16 11:02:49 +0100

    Created context file

---
+++ Data_Storage_Submission/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data_Storage_Submission.Context;
+
+internal class DataContext : DbContext
+{
+    protected DataContext()
+    {
+    }
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
