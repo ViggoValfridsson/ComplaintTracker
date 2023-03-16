209c6c5b63eb41dc48f5a99ba408d27498a258f7
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-03-16 14:13:30 +0100

    Created product service

---
+++ Data_Storage_Submission/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using Data_Storage_Submission.Context;
+using Data_Storage_Submission.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Data_Storage_Submission.Services;
+
+internal class ProductService : GenericServices<ProductEntity>
+{
+    private readonly DataContext _context = new DataContext();
+    public override async Task<ProductEntity> GetAsync(Expression<Func<ProductEntity, bool>> predicate)
+    {
+        var item = await _context.Products
+            .Include(x => x.Complaints)
+            .FirstOrDefaultAsync(predicate);
+
+        return item ?? null!;
+    }
+
+    public override async Task<ProductEntity> SaveAsync(ProductEntity entity)
+    {
+        var item = await GetAsync(x => x.Manufacturer == entity.Manufacturer && x.Name == entity.Name);
+
+        if (item == null)
+            return await base.SaveAsync(entity);
+
+        throw new ArgumentException("Product already exists in database.");
+    }
+}
