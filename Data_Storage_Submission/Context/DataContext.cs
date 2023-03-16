0155ac43565c7ebdecf0bae64c54a03f958e6a7a
Viggo Valfridsson <viggo.valfridsson@utb.ecutbildning.se>
2023-03-16 11:06:01 +0100

    Added dbsets to context

--- Data_Storage_Submission/Context/DataContext.cs
+++ Data_Storage_Submission/Context/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Data_Storage_Submission.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data_Storage_Submission.Context;
 
@@ -13,11 +14,20 @@ internal class DataContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\viggo\source\repos\Inlämningar\Data_Storage_Submission\Data_Storage_Submission\Context\submisson_db.mdf;Integrated Security=True;Connect Timeout=30");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
     }
+
+    public DbSet<DepartmentEntity> Departments { get; set; }
+    public DbSet<AddressEntity> Addresses { get; set; }
+    public DbSet<ProductEntity> Products { get; set; }
+    public DbSet<StatusTypeEntity> StatusTypes{ get; set; }
+    public DbSet<EmployeeEntity> Employees { get; set; }
+    public DbSet<CustomerEntity> Customers{ get; set; }
+    public DbSet<ComplaintEntity> Complaints { get; set; }
+    public DbSet<CommentEntity> Comments{ get; set; }
 }
